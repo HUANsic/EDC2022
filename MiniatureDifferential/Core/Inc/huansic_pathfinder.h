eda60b9b7e203393e746c5befa10960cd7af6c8f
HUANsic <wu-zh20@mails.tsinghua.edu.cn>
2022-11-10 19:27:40 +0800

    created pathfinder for MiniatureDifferential

---
+++ MiniatureDifferential/Core/Inc/huansic_pathfinder.h
@@ -0,0 +1,20 @@
+/*
+ * huansic_pathfinder.h
+ *
+ *  Created on: Nov 6, 2022
+ *      Author: Zonghuan Wu
+ */
+
+#ifndef INC_HUANSIC_PATHFINDER_H_
+#define INC_HUANSIC_PATHFINDER_H_
+
+#include "huansic_types.h"
+
+typedef struct Node_t{
+	Coordinate *location;		// 4
+	struct Node_t *lastNode;	// 4
+	float distance;				// 4
+	uint8_t processed;			// 1 (4)
+} Node;
+
+#endif /* INC_HUANSIC_PATHFINDER_H_ */
---
+++ MiniatureDifferential/Core/Src/huansic_pathfinder.c
@@ -0,0 +1,10 @@
+/*
+ * huansic_pathfinder.c
+ *
+ *  Created on: Nov 6, 2022
+ *      Author: Zonghuan Wu
+ */
+
+#include "huansic_pathfinder.h"
+
+
